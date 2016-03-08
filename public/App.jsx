c4a9acb148db34df864473a1d38f4e67a0f01c7d
juspears <juspears@paypal.com>
2016-03-07 17:47:13 -0800

    testing

--- public/App.jsx
+++ public/App.jsx
@@ -1,56 +1,57 @@
 "use strict";
 import React, {Component} from "react";
 import Subschema,{Form, loader, valueManager, loaderFactory} from "Subschema";
-
+import hello from 'hellojs';
+import Login from './Login.jsx';
 var schema = {
-	"schema": {
-		"title": {
-			"type": "Select",
-			"options": [
-				"Mr",
-				"Mrs",
-				"Ms"
-			]
-		},
-		"name": {
-			"type": "Text",
-			"validators": [
-				"required"
-			]
-		},
-		"age": {
-			"type": "Number"
-		}
-	},
-	"fieldsets": [
-		{
-			"legend": "Name",
-			"fields": "title, name, age",
-			"buttons": [
-				{
-					"label": "Cancel",
-					"action": "cancel",
-					"buttonClass": "btn"
-				},
-				{
-					"label": "Submit",
-					"action": "submit",
-					"buttonClass": "btn btn-primary"
-				}
-			]
-		}
-	]
+    "schema": {
+        "title": {
+            "type": "Select",
+            "options": [
+                "Mr",
+                "Mrs",
+                "Ms"
+            ]
+        },
+        "name": {
+            "type": "Text",
+            "validators": [
+                "required"
+            ]
+        },
+        "age": {
+            "type": "Number"
+        }
+    },
+    "fieldsets": [
+        {
+            "legend": "Name",
+            "fields": "title, name, age",
+            "buttons": [
+                {
+                    "label": "Cancel",
+                    "action": "cancel",
+                    "buttonClass": "btn"
+                },
+                {
+                    "label": "Submit",
+                    "action": "submit",
+                    "buttonClass": "btn btn-primary"
+                }
+            ]
+        }
+    ]
 };
 var value = {};
 
 
-
 export default class App extends Component {
-    render(){
+    render() {
         return <div>
             <h3>subschema-github</h3>
             <p></p>
+            <Login/>
             <Form schema={schema} value={value}/>;
-          </div>
+        </div>
     }
 }
